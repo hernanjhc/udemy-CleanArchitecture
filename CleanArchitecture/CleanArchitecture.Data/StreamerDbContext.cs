6926ae60f85f457ee178eeb51ab7dc1231ba2fb3
hernanjhc <hernan.jhc@gmail.com>
2023-01-09 20:47:13 -0300

    Creacion de modelos y migracion

---
+++ CleanArchitecture/CleanArchitecture.Data/StreamerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchitecture.Data
+{
+    public class StreamerDbContext : DbContext
+    {
+        //Cadena de conexion a sql server
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=localhost\sqlexpress; 
+                                    Initial Catalog = Streamer; 
+                                    Integrated Security=True;
+                                    TrustServerCertificate=True");
+        }
+
+        //Convierte clases en entidades
+        public DbSet<Streamer>? Streamers { get; set; }
+
+        public DbSet<Video>? Videos { get; set; }
+    }
+}
---
+++ CleanArchitecture/CleanArchitecture.Domain/Video.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain
+{
+    public class Video
+    {
+        public int Id { get; set; }
+        public string? Nonmbre { get; set; }
+
+        //Definicion clave foranea
+        //se crea columna que representa a streamer
+        public int StreamerId { get; set; }
+        public virtual Streamer? Streamer { get; set; }
+
+    }
+}
